db3cdf57ed16ce29b37a23622b4b203583761464
Yao Zongyou <yaozongyou@vip.qq.com>
2018-09-17 13:39:27 +0800

    add rbd doc

--- images/make_images.sh
+++ images/make_images.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+dot -Tpng -o rbd_image_request.png ./rbd_image_request.dot
 dot -Tpng -o rbd_object_request.png ./rbd_object_request.dot
--- rbd.md
+++ rbd.md
@@ -3,4 +3,5 @@
 
 ## 类之间的关系
 
+![rbd image request](images/rbd_image_request.png)
 ![rbd object request](images/rbd_object_request.png)
